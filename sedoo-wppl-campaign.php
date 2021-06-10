2bce9b4ed68c170cda0a2a2250a5ffc648aa0694
ngruwe <nicolas.gruwe@obs-mip.fr>
2021-06-10 17:08:28 +0200

    change tag

--- sedoo-wppl-campaign.php
+++ sedoo-wppl-campaign.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo - Campagnes
  * Description: Plugin nécessaire sur un wordpress type campagne
- * Version: 0.2.6
+ * Version: 0.2.7
  * Author: Nicolas Gruwe 
  * GitHub Plugin URI: sedoo/sedoo-wppl-campaign
  * GitHub Branch:     master
