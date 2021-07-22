71bc1353ff5e8c70e960e68a9dbe60f5649efbf1
Pierre Vert <pierr65@gmail.com>
2021-07-22 11:39:30 +0200

    fix bug on password required

--- sedoo-wppl-campaign.php
+++ sedoo-wppl-campaign.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo - Campaign product viewer
  * Description: Déclarer une campagne, ses produits et les viewers associés
- * Version: 0.4.0
+ * Version: 0.4.1
  * Author: Pierre Vert & Nicolas Gruwe 
  * GitHub Plugin URI: sedoo/sedoo-wppl-campaign
  * GitHub Branch:     master
